e0d4631e7c17ce33337acc23d65c51541c8ba699
Steven Zhu <stevenzhu03@gmail.com>
2020-01-09 23:08:13 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
+    #https://stackoverflow.com/questions/26146558/using-more-than-one-delimiter-for-split-method-in-ruby
+    self.split(/[.?!]/).reject {|string| string.size < 2}.length
 
   end
+
 end
